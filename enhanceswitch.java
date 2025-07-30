4c3a95f3c6c76741fd820ca4615f9971782dbf1a
Dishant singh rathore <dishantsinghrathore786@gmail.com>
2025-07-30 16:38:53 +0530

    feat: implement switch case for day evaluation

--- enhanceswitch.java
+++ enhanceswitch.java
@@ -1,7 +1,16 @@
 public class enhanceswitch {
     public static void main(String[] args){
+        String day = "monday";
+
+        switch (day) {
+            case "monday" -> System.out.println("its the first day of the week");
+                
+            default -> System.out.println("no the first day of the week ");
+                
+        }
         
+
     }
 
     
-}
+}  
