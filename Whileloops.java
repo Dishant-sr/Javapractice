181cc0a6cd37592a2f9706f53c71876df0dba7ca
Dishant singh rathore <dishantsinghrathore786@gmail.com>
2025-08-06 21:18:37 +0530

    feat: implement user input functionality in Whileloops class

--- Whileloops.java
+++ Whileloops.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 public class Whileloops {
     
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String name ;
+        System.out.println("enter your name:");
+        name = scanner.nextLine();
+        
+
+        
+        
+
+
         
     }
 }
