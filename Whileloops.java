2d4302f8c4a0dec16de5d91fdf243bfc60a0b522
Dishant singh rathore <dishantsinghrathore786@gmail.com>
2025-08-07 18:29:52 +0530

    fix: update prompt message for username input in Whileloops class

--- Whileloops.java
+++ Whileloops.java
@@ -4,9 +4,17 @@ public class Whileloops {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String name ;
-        System.out.println("enter your name:");
+        System.out.print("enter your username:");
         name = scanner.nextLine();
-        
+
+        while(name.contains("Dishantsr")){
+            System.out.println("Correct username");
+            break;
+           
+        }
+
+
+
 
         
         
