e39fd7e425aa03005f9d4f8092de0dd8ea4e4c38
Dishant singh rathore <dishantsinghrathore786@gmail.com>
2025-08-09 21:47:57 +0530

    feat: implement main method and add sample data to Hashmaps class

--- Hashmaps.java
+++ Hashmaps.java
@@ -1,3 +1,14 @@
+import java.util.HashMap;
 public class Hashmaps {
-    
+    public static void main(String[] args){
+        HashMap<String, Double> map = new HashMap<>();
+
+        map.put("apple", 0.25);
+        map.put("orange", 0.85);        
+        map.put("bananas", 0.55);
+
+        System.out.println(map);
+
+    }
+
 }
