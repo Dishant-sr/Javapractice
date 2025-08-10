c7528af1b54ea13f618609b9e8455579976855ef
Dishant singh rathore <dishantsinghrathore786@gmail.com>
2025-08-10 12:28:00 +0530

    feat: add initial implementation of birthday wish method and main class structure

---
+++ method.java
@@ -0,0 +1,19 @@
+public class method{
+
+static void birthdaywish(){
+    System.out.println("happy birthday bruce");
+    System.out.println("happy birthday batman ");
+}
+
+    public static void main(String[] args){
+        birthdaywish();
+        birthdaywish();
+        birthdaywish();
+        birthdaywish();
+        birthdaywish();
+        
+
+
+
+    }
+}
